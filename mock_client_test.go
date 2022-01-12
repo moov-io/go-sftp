2d8dadf9215355282c6cff583e7a3dd2b4d40118
Daniel Tonks <daniel@moov.io>
2022-01-12 16:03:58 -0700

    add path to mock client ListFiles()

--- mock_client_test.go
+++ mock_client_test.go
@@ -5,6 +5,7 @@
 package go_sftp_test
 
 import (
+	"io"
 	"io/ioutil"
 	"strings"
 	"testing"
@@ -32,5 +33,28 @@ func TestMockClient(t *testing.T) {
 	paths, err := client.ListFiles("/")
 	require.NoError(t, err)
 	require.Len(t, paths, 1)
-	require.Equal(t, "exists.txt", paths[0])
+	require.Equal(t, "/exists.txt", paths[0])
+}
+
+func TestMockClient_ListAndOpenFiles(t *testing.T) {
+	client := sftp.NewMockClient()
+	require.NoError(t, client.Ping())
+	defer require.NoError(t, client.Close())
+
+	require.NoError(t, client.UploadFile("/path/f1.txt", ioutil.NopCloser(strings.NewReader("foo"))))
+	require.NoError(t, client.UploadFile("/path/f2.txt", ioutil.NopCloser(strings.NewReader("foo"))))
+
+	foundFiles, err := client.ListFiles("/path/")
+	require.NoError(t, err)
+	require.Len(t, foundFiles, 2)
+
+	for _, file := range foundFiles {
+		found, err := client.Open(file)
+		require.NoError(t, err)
+		require.NotNil(t, found)
+
+		contents, err := io.ReadAll(found.Contents)
+		require.NoError(t, err)
+		require.Equal(t, "foo", string(contents))
+	}
 }
