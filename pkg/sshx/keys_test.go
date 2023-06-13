b5e02030f5ab25017d0d8e7a9d1a502c4bcadccf
Adam Shannon <adamkshannon@gmail.com>
2023-06-13 14:16:38 -0500

    sshx: remove outdated comment

--- pkg/sshx/keys_test.go
+++ pkg/sshx/keys_test.go
@@ -21,43 +21,13 @@ import (
 )
 
 func TestSSHX__read(t *testing.T) {
-	// TODO(adam): finish up with transform.GPGEncryption
-
-	// 	fd, err := os.Open(filepath.Join("testdata", "rsa-2048.pub"))
-	// 	if err != nil {
-	// 		t.Fatal(err)
-	// 	}
-	// 	t.Cleanup(func() { fd.Close() })
-
-	// 	block, err := armor.Decode(fd)
-	// 	if err != nil {
-	// 		t.Fatal(err)
-	// 	}
-	// 	if block.Type != openpgp.PublicKeyType {
-	// 		t.Fatal(err)
-	// 	}
-
-	// 	reader := packet.NewReader(block.Body)
-	// 	pkt, err := reader.Next()
-	// 	if err != nil {
-	// 		t.Fatal(err)
-	// 	}
-
-	// 	if key, ok := pkt.(*packet.PublicKey); !ok {
-	// 		t.Errorf("%T", pkt)
-	// 	} else {
-	// 		t.Logf("key=%#v", key)
-	// 	}
-
 	data, err := os.ReadFile(filepath.Join("testdata", "rsa-2048.pub"))
 	require.NoError(t, err)
 	key, err := ReadPubKey(data)
 	require.NoError(t, err)
 
 	if pk, ok := key.(ssh.CryptoPublicKey); ok {
-		t.Logf("ssh: pk=%#v", pk)
 		if pk, ok := pk.CryptoPublicKey().(*rsa.PublicKey); ok {
-			t.Logf("rsa: pk=%#v", pk)
 
 			var buf bytes.Buffer
 			w, err := armor.Encode(&buf, openpgp.PublicKeyType, make(map[string]string))
@@ -66,26 +36,13 @@ func TestSSHX__read(t *testing.T) {
 			}
 
 			pgpKey := packet.NewRSAPublicKey(time.Now(), pk)
-			t.Logf("pgpKey=%#v", pgpKey)
-
+			require.NotNil(t, pgpKey)
 			w.Close()
-
-			// to := createEntityFromKeys(pubKey, privKey)
-
-			// w, err := armor.Encode(os.Stdout, "Message", make(map[string]string))
-			// kingpin.FatalIfError(err, "Error creating OpenPGP Armor: %s", err)
-			// defer w.Close()
-
-			// plain, err := openpgp.Encrypt(w, []*openpgp.Entity{to}, nil, nil, nil)
-			// kingpin.FatalIfError(err, "Error creating entity for encryption")
-			// defer plain.Close()
 		}
 	}
 }
 
 func TestSSHX_ReadPubKey(t *testing.T) {
-	// TODO(adam): test with '-----BEGIN RSA PRIVATE KEY-----' PKCS#8 format
-
 	check := func(t *testing.T, data []byte) {
 		key, err := ReadPubKey(data)
 		if key == nil || err != nil {
